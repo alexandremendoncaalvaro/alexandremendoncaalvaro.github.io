e9d4f9585020774d9f4ea77e5bcb68a0097c7777
alexandremendoncaalvaro <alexandre.alvaro@hotmail.com>
2020-04-29 22:26:20 -0300

    Translating...

--- js/language.js
+++ js/language.js
@@ -35,6 +35,22 @@ const languageDictionary = {
                         href="https://www.hbsis.com.br" target="_blank">HBSIS Ambev</a>.<br>
                         I´m really excited about things that mix art with technology, like Motion Graphics, SVG animations,
                         music creation with my guitar and GarageBand, development of computer vision algorithms, and more.`,
+        'about-fullname-title': 'FULLNAME',
+        'about-birthdate-title': 'BIRTH DATE',
+        'about-birthdate': 'January 2, 1985',
+        'about-job-title': 'Job',
+        'about-job': `Researcher at <a href="https://www.hbsis.com.br" target="_blank">HBSIS Ambev</a><br>in Internet of Things <br>
+                        and Industrial Automation`,
+        'about-company-websites': 'Company Websites',
+        'about-hbsis-publication': 'HBSIS Ambev Publication at Linkedin (in Brazillian Portuguese)',
+        'about-skills': 'Skills Highlights',
+        'about-industrial-automation': 'Industrial Automation',
+        'about-plc': 'PLC (Rexroth, Siemens, Rockewell)',
+        'about-hmi': 'HMI (Allan Bradley, Siemens)',
+        'about-electrical': 'Eletrical Design',
+        'about-hydraulics': 'Hydraulics',
+        'about-pneumatics': 'Pneumatics',
+        'about-field': 'Commissioning, Field experience',
     },
     br: {
         'menu-intro': 'Início',
@@ -56,21 +72,41 @@ const languageDictionary = {
         'intro-areas-4': 'Inovação',
         'intro-comment': 'aqui você vai encontrar referências minhas para uma palestra ou freelance',
         'intro-more': 'Mais sobre mim',
-        'about-intro-title': 'Let me introduce myself.',
-        'about-intro': `I´m a guy who loves to learn. Human and Exacts sciences.
-                        <br>My professional experience is mainly focused on the design and development of industrial
-                        automation equipment and embedded software (PLC and microcontroller firmware development).
-                        <br>But I also have worked with graphical interface design, 3D modeling, illustration, and
-                        digital painting.
-                        <br>In my career, I could learn a lot about electrics, electronics, hydraulics, pneumatics,
-                        software development, and equipment commissioning.`,
-        'about-profile-title': 'PROFILE',
-        'about-profile': `I’m here for innovation, tech & beer!<br>I´m very proud to working for <a
-                        href="https://www.hbsis.com.br" target="_blank">HBSIS Ambev</a>, an
-                        <a href="https://www.ab-inbev.com" target="_blank">AB-Inbev</a> company.
-                        <br>I work as a technology researcher at LAHBS, the R&D department of <a
+        'about-intro-title': 'Deixa eu me apresentar.',
+        'about-intro': `Sou um cara que adora aprender. Independente de ser sobre humanas ou exatas.
+                        <br>Minha maior experiência é com o desenvolvimento e integração de softwares e equipamentos de 
+                        automação industrial de alta tecnologia, aplicativos e sistemas.
+                        Onde aprendi a lidar com projetos, metas, prazos, redução de custos, trabalho em equipe e lucratividade.
+                        Possuo também grande experiência em criação de interfaces gráficas e ilustrações digitais, 
+                        utilizadas em websites, aplicativos, IHMs e Sistemas supervisórios de alto padrão.
+                        <br>Durante este período pude atuar nas áreas gráficas, elétrica, eletrônica e programação,
+                        alternando entre estas áreas ou combinando-as em sistemas para web, apps, desenvolvimento de equipamentos,
+                        interfaces gráficas e outros.
+                        <br>Tenho excelente domínio em informática, projetos, desenhos técnicos e grande experiência prática com elétrica,
+                        hidráulica, pneumática, instrumentação, comissionamento e startup de equipamentos industriais.`,
+        'about-profile-title': 'PERFIL',
+        'about-profile': `<i>I’m here for innovation, tech & beer!</i><br>Tenho muito orgulho de trabalhar na <a
+                        href="https://www.hbsis.com.br" target="_blank">HBSIS Ambev</a>, empresa do grupo
+                        <a href="https://www.ab-inbev.com" target="_blank">AB-Inbev</a>.
+                        <br>Eu atuo como pesquisador de tecnologia e inovação no LAHBS, o departamento de P&D da <a
                         href="https://www.hbsis.com.br" target="_blank">HBSIS Ambev</a>.<br>
-                        I´m really excited about things that mix art with technology, like Motion Graphics, SVG animations,
-                        music creation with my guitar and GarageBand, development of computer vision algorithms, and more.`,
+                        Sou apaixonado por misturar disciplinas de tecnologia e arte, como Motion Graphics, animações SVG,
+                        criação de música com meu violão e o GarageBand, desenvolvimento de algoritmos de visão computacional, entre outros.`,
+        'about-fullname-title': 'NOME',
+        'about-birthdate-title': 'NASCIMENTO',
+        'about-birthdate': '2 de janeiro de 1985',
+        'about-job-title': 'Profissão',
+        'about-job': `Pesquisador na <a href="https://www.hbsis.com.br" target="_blank">HBSIS Ambev</a><br>em Internet das Coisas <br>
+                        e Automação Industrial`,
+        'about-company-websites': 'Sites da Companhia',
+        'about-hbsis-publication': 'Publicação da HBSIS Ambev no Linkedin',
+        'about-skills': 'Conhecimentos em destaque',
+        'about-industrial-automation': 'Automação Industrial',
+        'about-plc': 'CLP (Rexroth, Siemens, Rockewell)',
+        'about-hmi': 'IHM (Allan Bradley, Siemens)',
+        'about-electrical': 'Projetos Elétricos',
+        'about-hydraulics': 'Hidráulica',
+        'about-pneumatics': 'Pneumatica',
+        'about-field': 'Comissionamento,<br>Experiência de campo',
     }
 }
